e33ff648f17aefd0ceaa04b0c1f28ce4e2a06e93
Yogesh Prabhukhanolkar <yogeshprabhums@gmail.com>
2018-10-25 19:11:39 -0400

    Added Reset and delete functionality

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,28 +1,19 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    value: this.props.value
-  };
-
-  constructor(props) {
-    super(props);
-    this.handleIncrement = this.handleIncrement.bind(this);
-  }
   render() {
-    console.log(this.props);
     return (
       <div>
-        <span className={this.getbadgeClasses()}>{this.state.value}</span>
+        <span className={this.getbadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm m-2"
         >
           Increment
         </button>
         <button
           className="btn btn-danger btn-sm m-2"
-          onClick={this.props.onDelete}
+          onClick={() => this.props.onDelete(this.props.counter.id)}
         >
           Delete
         </button>
@@ -31,14 +22,13 @@ class Counter extends Component {
   }
   getbadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    //const { value } = this.state.value;
-    //return value === 0 ? "Zero" : value;
-    //return ({this.state.value});
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
   }
 
   rendertags() {
@@ -48,12 +38,6 @@ class Counter extends Component {
       return this.state.tags.map(tag => <li key={tag}>{tag}</li>);
     }
   }
-
-  handleIncrement = () => {
-    //console.log(product);
-    //console.log("This is class'  property", this);
-    this.setState({ value: this.state.value + 1 });
-  };
 }
 
 export default Counter;
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -9,17 +9,43 @@ class Counters extends Component {
       { id: 4, value: 0 }
     ]
   };
-  handleDelete = () => {
+  handleDelete = counterId => {
+    console.log("In delete");
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters });
     console.log("event raised");
+  };
+  handleIncrement = counter => {
+    console.log("In increment");
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+    //console.log(counters[0]);
+  };
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
   };
   render() {
     return (
       <div>
+        <button
+          className="btn btn-primary btn-sm m-2"
+          onClick={this.handleReset}
+        >
+          Reset
+        </button>
         {this.state.counters.map(counter => (
           <Counter
             key={counter.id}
-            value={counter.value}
+            counter={counter}
             onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
           />
         ))}
       </div>
